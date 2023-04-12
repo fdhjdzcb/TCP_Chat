fd0be389bf1b4bde3e73d6c18a723607e4163dc0
Amir <fdhjdzcb@gmail.com>
2023-04-12 15:58:13 +0300

    написаны логи уровня info и warning

--- code/src/server.cpp
+++ code/src/server.cpp
@@ -12,103 +12,113 @@
 #include <glog/logging.h>
 #include <sys/stat.h>
 
-struct Message{
-    char* msg;
+struct Message {
+    char *msg;
     size_t msg_size;
 };
 
 pthread_t thread_id1;
 std::unordered_map<int, std::string> Connections;
 
-//class Disconnect : public std::exception { };
-
-Message stringToChar(std::string &msg){
+Message stringToChar(std::string &msg) {
     size_t msg_size = msg.size();
     char *cmsg = new char[msg_size + 1];
     strcpy(cmsg, msg.c_str());
     cmsg[msg_size] = '\0';
+
+    LOG(INFO) << "Сообщение преобразовано из std::string в структуру Message:"
+              << "\nСообщение: " << cmsg
+              << "\nРазмер: " << msg_size + 1
+              << "\n";
+
     return {cmsg, msg_size + 1};
 }
 
-std::string receiveMsgFromClient(int socketID){
+std::string receiveMsgFromClient(int socketID) {
     int msg_size;
     int recvRes = recv(socketID, (char *) &msg_size, sizeof(int), 0);
 
     if (recvRes == 0) {
         throw std::runtime_error(std::to_string(socketID) + " disconnected");
     }
-    std::cout << "recvRes^^^^^^^^ " << recvRes << std::endl;
 
     char *msg = new char[msg_size + 1];
     recv(socketID, msg, msg_size, 0);
     msg[msg_size] = '\0';
+
+    LOG(INFO) << "Получено сообщение с сокета " << socketID
+              << "\nОт клиента: " << Connections[socketID]
+              << "\nРезультат recv: " << recvRes
+              << "\nСообщение: " << msg;
+
     return msg;
 };
 
-void sendMsgToClients(std::string &msg){
+void sendMsgToClients(std::string &msg) {
     auto newMsg = stringToChar(msg);
-    for (auto [socketNum, username] : Connections) {
+    for (auto [socketNum, username]: Connections) {
         send(socketNum, (char *) &newMsg.msg_size, sizeof(int), 0);
         send(socketNum, newMsg.msg, newMsg.msg_size, 0);
-        //std::cout << newMsg.msg << " for " << username << std::endl;
     }
+    LOG(INFO) << "Всем клиентам отправлено сообщение: " << newMsg.msg;
 };
 
-void welcome(int index){
+void welcome(int socketID) {
     std::string welcomeMsg = "Привет! Сейчас в чате:";
-    for (const auto& conn : Connections){
-        /*if (i == index){
-            continue;
-        }*/
+    for (const auto &conn: Connections) {
         welcomeMsg.append(" ");
         welcomeMsg.append(conn.second);
         welcomeMsg.append(",");
     }
     welcomeMsg[welcomeMsg.length() - 1] = '!';
-    std::cout << welcomeMsg << std::endl;
+    LOG(INFO) << "Клиенту на сокете " << socketID << " отправлено приветственное сообщение:\n" << welcomeMsg;
     auto newMsg = stringToChar(welcomeMsg);
-    send(index, (char *) &newMsg.msg_size, sizeof(int), 0);
-    send(index, newMsg.msg, newMsg.msg_size, 0);
+    send(socketID, (char *) &newMsg.msg_size, sizeof(int), 0);
+    send(socketID, newMsg.msg, newMsg.msg_size, 0);
 }
 
-void *clientHandler(void *arg){
+void *clientHandler(void *arg) {
     int socketID = *((int *) arg);
 
-    try{
+    try {
         std::string username = receiveMsgFromClient(socketID);
         Connections[socketID] = username;
         welcome(socketID);
+        LOG(INFO) << "Клиенту на сокете " << socketID << " присвоено имя " << username;
 
-        while (true){
+        while (true) {
             std::string msg = Connections[socketID];
             msg.append(": ");
-            msg.append(receiveMsgFromClient(socketID));
+            std::string msgFromClient = receiveMsgFromClient(socketID);
+            msg.append(msgFromClient);
+
+            LOG(INFO) << "От клиента " << Connections[socketID] << " на сокете " << socketID << " получено сообщение:\n"
+                      << msgFromClient;
+
             sendMsgToClients(msg);
         }
     }
-    catch (const std::runtime_error& e){
+    catch (const std::runtime_error &e) {
+        LOG(WARNING) << "Клиент " << Connections[socketID] << " с сокета " << socketID << " отключен";
         shutdown(socketID, SHUT_RDWR);
         close(socketID);
         std::cerr << e.what() << std::endl;
-        //std::cerr << Connections[socketID] << " disconnected\n";
         Connections.erase(socketID);
-        for (const auto& conn : Connections){
-            std::cout << "start " << conn.first << " " << conn.second << " end\n";
-        }
+        LOG(INFO) << "Сокет " << socketID << " закрыт";
         return nullptr;
     }
 }
 
 int main() {
-    //mkdir("/logs", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
     google::InitGoogleLogging("Server");
+    mkdir("logs", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
     FLAGS_log_dir = "logs";
     FLAGS_logtostderr = false;
-    google::SetLogDestination(google::INFO, "log_file");
+    FLAGS_stderrthreshold = 3;
+    FLAGS_logbufsecs = 1;
+    google::SetLogDestination(google::INFO, "logs/log_file");
 
-    LOG(WARNING) << "Это предупреждение.";
-    LOG(ERROR) << "Это сообщение об ошибке.";
-    LOG(INFO) << "Это информационное сообщение.";
+    LOG(INFO) << "Сервер начал работу";
 
     sockaddr_in addr{};
     socklen_t sizeOfAddr = sizeof(addr);
@@ -118,27 +128,32 @@ int main() {
 
     int sListen = socket(AF_INET, SOCK_STREAM, 0);
     int optval = 1;
-    setsockopt(sListen,SOL_SOCKET,SO_REUSEADDR,&optval,sizeof(optval));
+    setsockopt(sListen, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
     bind(sListen, (sockaddr *) &addr, sizeOfAddr);
     std::cout << "Waiting for client...\n";
     listen(sListen, SOMAXCONN);
     std::cout << "ListenSocketID: " << sListen << std::endl;
+    LOG(INFO) << "Сервер слушает сокет " << sListen;
+
 
     int newConnection;
-    while(true) {
+    while (true) {
         newConnection = accept(sListen, (sockaddr *) &addr, &sizeOfAddr);
         std::cout << "Connection socket: " << newConnection << std::endl;
 
         if (newConnection == 0) {
             std::cout << "Client connection error\n";
+            LOG(ERROR) << "Ошибка подключения клиента";
         } else {
             Connections[newConnection] = "name";
-            for (auto conn : Connections){
+            for (auto conn: Connections) {
                 std::cout << "map: " << conn.first << " " << conn.second << std::endl;
             }
             pthread_create(&thread_id1, nullptr, &clientHandler, &newConnection);
             std::cout << "Client connected! Num of clients: " << Connections.size() << "\n";
+            LOG(INFO) << "Клиент подключен к сокету " << newConnection;
+            LOG(INFO) << "Общее количество клиентов: " << Connections.size();
         }
     }
 }
